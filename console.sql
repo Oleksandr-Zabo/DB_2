5bddfb4df4e3fb102f980ab6f25363a6b07addd1
Oleksandr-Zabo <zoleksandr06@gmail.com>
2025-02-09 21:42:16 +0200

    hw had done

--- console.sql
+++ console.sql
@@ -1,110 +1,159 @@
-﻿create database HospitalDB;
+﻿create database Academy;
 go
-use HospitalDB;
+use Academy;
 
+--create tables
 go
-create table Doctor
+create table Groups
 (
-    DoctorID int primary key identity(1,1),
-    DoctorName varchar(50) check(DoctorName like '[A-Z]%') not null,
-    DoctorSpeciality varchar(50) check(DoctorSpeciality like '[A-Z]%'),
-    DoctorSalary money check(DoctorSalary > 0) default 3500
-);
-go
-create table CountryCode
-(
-    CountryCodeID int primary key identity(1,1),
-    CountryCode varchar(50) check(CountryCode like '[0-9]%') not null,
-    CountryName varchar(50) check(CountryName like '[A-Z]%') not null
+    Id int primary key identity (1,1) not null,
+    Name nvarchar(10) NOT NULL UNIQUE check (LEN(Name) > 0),
+    Rating int not null check (Rating >= 0 and Rating <= 5),
+    Year int not null check (Year >= 1 and Year <= 5)
 );
 
 go
-create table Patient
+create table Departments
 (
-    PatientID int primary key identity(1,1),
-    PatientName varchar(50) check(PatientName like '[A-Z]%') not null,
-    PatientAge int check(PatientAge > 0 and PatientAge < 120) not null,
-    PatientGender varchar(50),
-    PatientAddress varchar(100) check(PatientAddress like '[A-Z]%') not null,
-    CountryCodeID int foreign key references CountryCode(CountryCodeID),
-    PatientPhone varchar(50) check(PatientPhone like '[0-9]%') not null,
-    PatientEmail nvarchar(50) check (PatientEmail like '%@%.%') not null
-);
-go
-create table Appointment
-(
-    AppointmentID int primary key identity(1,1),
-    AppointmentDate date not null default getdate() check (MONTH(AppointmentDate) = MONTH(getdate()) and YEAR(AppointmentDate) = YEAR(getdate()) or AppointmentDate > getdate()),
-    AppointmentTime time not null,
-    DoctorID int foreign key references Doctor(DoctorID) check (DoctorID > 0),
-    PatientID int foreign key references Patient(PatientID) check (PatientID > 0)
+    Id int primary key identity (1,1) not null,
+    Financing money not null check (Financing >= 0) default 0,
+    Name nvarchar(100) NOT NULL UNIQUE check (LEN(Name) > 0),
 );
 
-
 go
-create table Prescription
-(
-    PrescriptionID int primary key identity(1,1),
-    PrescriptionDate date not null default getdate() check (MONTH(PrescriptionDate) = MONTH(getdate()) and YEAR(PrescriptionDate) = YEAR(getdate()) or PrescriptionDate > getdate()),
-    PrescriptionTime time not null,
-    DoctorID int foreign key references Doctor(DoctorID) check (DoctorID > 0),
-    PatientID int foreign key references Patient(PatientID) check (PatientID > 0),
-    PrescriptionDetails varchar(100) check(PrescriptionDetails like '[A-Z]%') not null
+create table Faculties(
+                          Id int primary key identity (1,1) not null,
+                          Dean nvarchar(max) NOT NULL check (LEN(Dean) > 0),
+                          Name nvarchar(100) NOT NULL UNIQUE check (LEN(Name) > 0)
 );
--- Insert into Doctor
-insert into Doctor (DoctorName, DoctorSpeciality, DoctorSalary) values
-                                                                    ('Dr. John', 'Cardiologist', 5000),
-                                                                    ('Dr. Smith', 'Dentist', 4000),
-                                                                    ('Dr. Alex', 'Pediatrician', 4500),
-                                                                    ('Dr. Maria', 'Gynecologist', 5000),
-                                                                    ('Dr. David', 'Orthopedic', 5500),
-                                                                    ('Dr. Sarah', 'Dermatologist', 4500);
-
--- Insert into CountryCode
-insert into CountryCode (CountryCode, CountryName) values
-                                                       ('1', 'USA'),
-                                                       ('44', 'UK'),
-                                                       ('91', 'India'),
-                                                       ('61', 'Australia'),
-                                                       ('64', 'New Zealand'),
-                                                       ('27', 'South Africa');
 
 go
--- Insert into Patient
-insert into Patient (PatientName, PatientAge, PatientGender, PatientAddress, CountryCodeID, PatientPhone, PatientEmail) values
-                                                                                                                            ('Alice', 30, 'Female', 'Main St, 123', 1, '1234567890', 'alice@example.com'),
-                                                                                                                            ('Bob', 45, 'Male', 'Elm St, 456', 2, '2345678901', 'bob@example.com'),
-                                                                                                                            ('Charlie', 28, 'Male', 'Oak St, 789', 3, '3456789012', 'charlie@example.com'),
-                                                                                                                            ('Diana', 35, 'Female', 'Pine St, 101', 4, '4567890123', 'diana@example.com'),
-                                                                                                                            ('Eve', 40, 'Female', 'Maple St, 202', 5, '5678901234', 'eve@example.com');
--- Insert into Appointment
-insert into Appointment (AppointmentDate, AppointmentTime, DoctorID, PatientID) values
-                                                                                    ('2025-10-01', '10:00:00', 1, 1),
-                                                                                    ('2025-10-02', '11:00:00', 2, 2),
-                                                                                    ('2025-10-03', '12:00:00', 3, 3),
-                                                                                    ('2025-10-04', '13:00:00', 4, 4),
-                                                                                    ('2025-10-05', '14:00:00', 5, 5);
+create table Teachers
+(
+    Id int primary key identity (1,1) not null,
+    EmploymentDate date not null check (EmploymentDate <= '1990-01-01'),
+    IsAssistant bit not null default 0,
+    IsProfessor bit not null default 0,
+    Name nvarchar(max) NOT NULL check (LEN(Name) > 0),
+    Premium money not null check (Premium >= 0) default 0,
+    Salary money not null check (Salary >= 0),
+    Surname nvarchar(max) NOT NULL check (LEN(Surname) > 0),
+);
 
--- Insert into Prescription
-insert into Prescription (PrescriptionDate, PrescriptionTime, DoctorID, PatientID, PrescriptionDetails) values
-                                                                                                            ('2025-10-01', '10:00:00', 1, 1, 'Prescription for Alice'),
-                                                                                                            ('2025-10-02', '11:00:00', 2, 2, 'Prescription for Bob'),
-                                                                                                            ('2025-10-03', '12:00:00', 3, 3, 'Prescription for Charlie'),
-                                                                                                            ('2025-10-04', '13:00:00', 4, 4, 'Prescription for Diana'),
-                                                                                                            ('2025-10-05', '14:00:00', 5, 5, 'Prescription for Eve');
---select
+--inserts
 go
-select * from Doctor;
-select * from CountryCode;
-select * from Patient;
-select * from Appointment;
-select * from Prescription;
+insert into Groups (Name, Rating, Year) values
+                                            ('Group1', 3, 1),
+                                            ('Group2', 4, 2),
+                                            ('Group3', 2, 3),
+                                            ('Group4', 5, 4),
+                                            ('Group5', 4, 5);
+go
+insert into Departments (Financing, Name) values
+                                              (10000, 'Department1'),
+                                              (20000, 'Department2'),
+                                              (15000, 'Department3'),
+                                              (25000, 'Department4'),
+                                              (30000, 'Department5');
 
---drop
-drop table Doctor;
-drop table CountryCode;
-drop table Patient;
-drop table Appointment;
-drop table Prescription;
+go
+insert into Faculties (Dean, Name) values
+                                       ('Dean1', 'Faculty1'),
+                                       ('Dean2', 'Faculty2'),
+                                       ('Dean3', 'Faculty3'),
+                                       ('Dean4', 'Faculty4'),
+                                       ('Dean5', 'Faculty5');
+
+go
+insert into Teachers (EmploymentDate, IsAssistant, IsProfessor, Name, Premium, Salary, Surname) values
+                                                                                                    ('1985-01-01', 1, 0, 'Teacher1', 199, 1000, 'Smith'),
+                                                                                                    ('1987-01-01', 0, 1, 'Teacher2', 300, 1500, 'Johnson'),
+                                                                                                    ('1989-01-01', 1, 0, 'Teacher3', 250, 1200, 'Williams'),
+                                                                                                    ('1986-01-01', 0, 1, 'Teacher4', 400, 2000, 'Brown'),
+                                                                                                    ('1988-01-01', 1, 0, 'Teacher5', 350, 1800, 'Jones');
+
+--selects
+go
+select Name, Financing, Id
+from Departments;--1
+
+
+go
+select Name as "Group Name", Rating as "Group Rating"
+from Groups;--2
+
+go
+select Surname,
+       (Premium / Salary) * 100 as "Premium Percentage",
+       (Salary / (Salary + Premium)) * 100 as "Salary Percentage"
+from Teachers;--3
+
+go
+select 'The dean of faculty ' + Name + ' is ' + Dean as "Faculty Info"
+from Faculties;--4
+
+go
+select Surname
+from Teachers
+where IsProfessor = 1 and Salary > 1050;--5
+
+go
+select Name
+from Departments
+where Financing < 11000 or Financing > 25000;--6
+
+go
+select Name
+from Faculties
+where Name != 'Computer Science';--7
+
+go
+select Surname, 'Assistant' as Position
+from Teachers
+where IsProfessor = 0;--8
+
+go
+select Surname, 'Assistant' as Position, Salary, Premium
+from Teachers
+where IsAssistant = 1 and Premium between 160 and 550;--9
+
+go
+select Surname, Salary
+from Teachers
+where IsAssistant = 1;--10
+
+go
+select Surname, 'Assistant' as Position
+from Teachers
+where EmploymentDate < '2000-01-01';--11
+
+go
+select Name as "Name of Department"
+from Departments
+where Name < 'Software Development'
+order by Name;--12
+
+go
+select Surname
+from Teachers
+where IsAssistant = 1 and (Salary + Premium) <= 1200;--13
+
+go
+select Name
+from Groups
+where Year = 5 and Rating between 2 and 4;--14
+
+go
+select Surname
+from Teachers
+where IsAssistant = 1 and (Salary < 550 or Premium < 200);--15
+
+--deletes
+go
+drop table Teachers;
+drop table Faculties;
+drop table Departments;
+drop table Groups;
+
 use master;
-drop database HospitalDB;
+drop database Academy;
